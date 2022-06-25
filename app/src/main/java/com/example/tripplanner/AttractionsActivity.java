3333d78324cd6c11a7d00dca481475004297ded1
Tinatin Kokoshvili <tinatink@fb.com>
2022-06-24 17:08:32 -0700

    Resolved some minor errors

--- app/src/main/java/com/example/tripplanner/AttractionsActivity.java
+++ app/src/main/java/com/example/tripplanner/AttractionsActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AttractionsActivity extends AppCompatActivity {
-    AmadeusClient amClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
