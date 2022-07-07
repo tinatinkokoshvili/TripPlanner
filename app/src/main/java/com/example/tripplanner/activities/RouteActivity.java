58179d278cbd870215c18362f89773f5a2dd2f3f
Tinatin Kokoshvili <tinatink@fb.com>
2022-07-07 18:25:13 -0400

    implemented Fetchurl and PointsParser classes, displaying the polyline and markers on the map

--- app/src/main/java/com/example/tripplanner/activities/RouteActivity.java
+++ app/src/main/java/com/example/tripplanner/activities/RouteActivity.java
@@ -1,78 +1,58 @@
 package com.example.tripplanner.activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 import com.example.tripplanner.OnTaskCompleted;
 import com.example.tripplanner.R;
 import com.example.tripplanner.RouteGenerator;
 import com.example.tripplanner.apiclient.DistanceMatrixHelper;
+import com.example.tripplanner.directionhelpers.FetchURL;
+import com.example.tripplanner.directionhelpers.TaskLoadedCallback;
 import com.example.tripplanner.models.Attraction;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.model.AutocompletePrediction;
-import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.IntentSender;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.location.Location;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
-import okhttp3.Route;
 
-public class RouteActivity extends AppCompatActivity implements OnTaskCompleted, OnMapReadyCallback {
+public class RouteActivity extends AppCompatActivity implements OnTaskCompleted, OnMapReadyCallback, TaskLoadedCallback {
     private static final String TAG = "RouteActivity";
     private List<Attraction> pickedAtrList;
     private static final String distanceMatrixBaseUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric";
+    private static final String directionsBaseUrl = "https://maps.googleapis.com/maps/api/directions/";
     private static final String API_KEY = "AIzaSyCe2kjKuINrKzh9bvmGa-ToZiEvluGRzwU";
+    private static final String TRAVEL_MODE = "driving";
     private List<Attraction> atrRoute;
     private int[][] durationMatrix;
     private double userLatitude;
     private double userLongitude;
 
-    private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-    private PlacesClient placesClient;
-    private List<AutocompletePrediction> predictionList;
-
-    private Location mLastKnownLocation;
-    private LocationCallback locationCallback;
     private View mapView;
 
-    private LatLng latLngOfPlace;
+    private final float DEFAULT_ZOOM = 12;
 
-    private final float DEFAULT_ZOOM = 18;
+    private GoogleMap mMap;
+    private Polyline currentPolyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,7 +71,7 @@ public class RouteActivity extends AppCompatActivity implements OnTaskCompleted,
         Log.i(TAG, "map " + mapFragment);
         mapFragment.getMapAsync(this);
         mapView = mapFragment.getView();
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(RouteActivity.this);
+       // mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(RouteActivity.this);
     }
 
     private void fetchDistances() {
@@ -123,7 +103,7 @@ public class RouteActivity extends AppCompatActivity implements OnTaskCompleted,
         stringBuilder.append(Double.toString(userLongitude));
 
         stringBuilder.append("&departure_time=now");
-        stringBuilder.append("&key=" + API_KEY);
+        stringBuilder.append("&key=" + getString(R.string.google_maps_key));
         String url = stringBuilder.toString();
         Object dataTransfer[] = new Object[1];
         dataTransfer[0] = url;
@@ -140,12 +120,26 @@ public class RouteActivity extends AppCompatActivity implements OnTaskCompleted,
         this.durationMatrix = durationMatrix;
         // Add current location to the list of picked attractions
         Attraction userLocation = new Attraction();
+        userLocation.name = "User Location";
         userLocation.latitude = Double.toString(userLatitude);
         userLocation.longitude = Double.toString(userLongitude);
         pickedAtrList.add(userLocation);
         RouteGenerator rtGenerator = new RouteGenerator(pickedAtrList, durationMatrix);
-        List<Attraction> atrRoute = rtGenerator.getRouteList();
-        // Display the route
+        atrRoute = rtGenerator.getRouteList();
+        // Add markers to the map
+        addMarkers();
+        // Add routes to the map
+        Log.i(TAG, "atrRoute size " + atrRoute.size());
+
+        for (int i = 0; i < atrRoute.size() - 1; i++) {
+            Log.i(TAG, "atrRoute size " + atrRoute.get(i).name);
+            MarkerOptions origin = new MarkerOptions().position(new LatLng(Double.parseDouble(atrRoute.get(i).latitude),
+                    Double.parseDouble(atrRoute.get(i).longitude))).title(atrRoute.get(i).name);
+            MarkerOptions destination = new MarkerOptions().position(new LatLng(Double.parseDouble(atrRoute.get(i + 1).latitude),
+                    Double.parseDouble(atrRoute.get(i + 1).longitude))).title(atrRoute.get(i + 1).name);
+            new FetchURL(RouteActivity.this)
+                    .execute(getUrl(origin.getPosition(), destination.getPosition(), TRAVEL_MODE), TRAVEL_MODE);
+        }
     }
 
     public static byte[] convert(Bitmap bitmap) throws IOException {
@@ -177,99 +171,34 @@ public class RouteActivity extends AppCompatActivity implements OnTaskCompleted,
             layoutParams.setMargins(0, 0, 40, 180);
         }
 
-        //check if gps is enabled or not and then request user to enable it
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder =
-                new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-
-        SettingsClient settingsClient = LocationServices.getSettingsClient(RouteActivity.this);
-        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener(RouteActivity.this, new OnSuccessListener<LocationSettingsResponse>() {
-            @Override
-            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                getDeviceLocation();
-            }
-        });
-
-        task.addOnFailureListener(RouteActivity.this, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof ResolvableApiException) {
-                    ResolvableApiException resolvable = (ResolvableApiException) e;
-                    try {
-                        //let user enable location
-                        resolvable.startResolutionForResult(RouteActivity.this, 51);
-                    } catch (IntentSender.SendIntentException e1) {
-                        e1.printStackTrace();
-                    }
-                }
-            }
-        });
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(userLatitude, userLongitude), DEFAULT_ZOOM));
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 51) {
-            //check if user enabled location
-            if (resultCode == RESULT_OK) {
-                getDeviceLocation();
-            }
+    private void addMarkers() {
+        for (int i = 0; i < atrRoute.size(); i++) {
+            Log.i(TAG, "lat string " + atrRoute.get(i).latitude + " long string " + atrRoute.get(i).longitude
+                    + " double " + Double.parseDouble(atrRoute.get(i).latitude) + " double long " + Double.parseDouble(atrRoute.get(i).longitude));
+            MarkerOptions place =
+                new MarkerOptions().position(new LatLng(Double.parseDouble(atrRoute.get(i).latitude),
+                        Double.parseDouble(atrRoute.get(i).longitude))).title(atrRoute.get(i).name);
+            mMap.addMarker(place);
         }
     }
 
-    //Supressing the permission error for now, user needs to give permission to the app to use location
-    @SuppressLint("MissingPermission")
-    private void getDeviceLocation() {
-        //Fetch current location of decide
-        mFusedLocationProviderClient.getLastLocation()
-                .addOnCompleteListener(new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            mLastKnownLocation = task.getResult();
-                            if (mLastKnownLocation != null) {
-                                //Move map to the current location
-                                LatLng mLastKnownLocationCoord = new LatLng(mLastKnownLocation.getLatitude(),
-                                        mLastKnownLocation.getLongitude());
-                                //Set the variable so that we can pass it into pickActivity intent if searchbar not used
-                                latLngOfPlace = mLastKnownLocationCoord;
-                                Log.i(TAG, "latlong in getDeviceLocatoin " + latLngOfPlace.latitude + " " + latLngOfPlace.longitude);
-                                mMap.moveCamera(CameraUpdateFactory
-                                        .newLatLngZoom(mLastKnownLocationCoord, DEFAULT_ZOOM));
-                            } else {
-                                //Request for updated location
-                                final LocationRequest locationRequest = LocationRequest.create();
-                                locationRequest.setInterval(10000);
-                                locationRequest.setFastestInterval(5000);
-                                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                                locationCallback = new LocationCallback() {
-                                    @Override
-                                    public void onLocationResult(LocationResult locationResult) {
-                                        super.onLocationResult(locationResult);
-                                        if (locationResult == null) {
-                                            return;
-                                        }
-                                        mLastKnownLocation = locationResult.getLastLocation();
-                                        mMap.moveCamera(CameraUpdateFactory
-                                                .newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(),
-                                                        mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                                        mFusedLocationProviderClient.removeLocationUpdates(locationCallback);
-                                    }
-                                };
-                                mFusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        String mode = "mode=" + directionMode;
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = directionsBaseUrl + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
 
-                            }
-                        } else {
-                            Toast.makeText(RouteActivity.this, "Unable to get last location", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+    @Override
+    public void onTaskDone(Object... values) {
+        currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
     }
 }
---
+++ app/src/main/java/com/example/tripplanner/directionhelpers/TaskLoadedCallback.java
@@ -0,0 +1,5 @@
+package com.example.tripplanner.directionhelpers;
+
+public interface TaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
