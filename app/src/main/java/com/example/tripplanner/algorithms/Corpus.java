c6870b946fb024e2747eeb2281351a94289da8ab
Tinatin Kokoshvili <tinatink@fb.com>
2022-07-19 15:54:23 -0700

    Created Corpus class that will create inverted index for these documents, and deleted (Yelp)NearbyRestaurantsHelper

---
+++ app/src/main/java/com/example/tripplanner/algorithms/Corpus.java
@@ -0,0 +1,58 @@
+package com.example.tripplanner.algorithms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * This class represents a corpus of documents and will create an inverted index for these documents.
+ */
+public class Corpus {
+    private ArrayList<Document> documents;
+    private HashMap<String, Set<Document>> invertedIndex;
+
+    public Corpus(ArrayList<Document> documents) {
+        this.documents = documents;
+        invertedIndex = new HashMap<String, Set<Document>>();
+
+        createInvertedIndex();
+    }
+
+    private void createInvertedIndex() {
+        for (Document document : documents) {
+            Set<String> terms = document.getTermList();
+
+            for (String term : terms) {
+                if (invertedIndex.containsKey(term)) {
+                    Set<Document> list = invertedIndex.get(term);
+                    list.add(document);
+                } else {
+                    Set<Document> list = new TreeSet<Document>();
+                    list.add(document);
+                    invertedIndex.put(term, list);
+                }
+            }
+        }
+    }
+
+    public double getInverseDocumentFrequency(String term) {
+        if (invertedIndex.containsKey(term)) {
+            double size = documents.size();
+            Set<Document> list = invertedIndex.get(term);
+            double documentFrequency = list.size();
+
+            return Math.log10(size / documentFrequency);
+        } else {
+            return 0;
+        }
+    }
+
+    public ArrayList<Document> getDocuments() {
+        return documents;
+    }
+
+    public HashMap<String, Set<Document>> getInvertedIndex() {
+        return invertedIndex;
+    }
+}
