ae50ad690161ed463b7bbdf2ea471b72bd84b27f
Tinatin Kokoshvili <tinatink@fb.com>
2022-07-20 10:36:08 -0700

    Created VectorSpaceModel that computes the cosine similarity

---
+++ app/src/main/java/com/example/tripplanner/algorithms/VectorSpaceModel.java
@@ -0,0 +1,88 @@
+package com.example.tripplanner.algorithms;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * This class takes a corpus and creates the tf-idf vectors for each document and computes
+ * the cosine similarity between documents.
+ */
+public class VectorSpaceModel {
+    private static final String TAG = "VectorSpaceModel";
+    private Corpus corpus;
+
+    /**
+     * The tf-idf weight vectors.
+     * The hashmap maps a document to another hashmap.
+     * The second hashmap maps a term to its tf-idf weight for this document.
+     */
+    private HashMap<Document, HashMap<String, Double>> tfIdfWeights;
+
+    /**
+     * Using the corpus, it will generate tf-idf vectors for each document.
+     */
+    public VectorSpaceModel(Corpus corpus) {
+        this.corpus = corpus;
+        tfIdfWeights = new HashMap<Document, HashMap<String, Double>>();
+
+        createTfIdfWeights();
+    }
+
+    private void createTfIdfWeights() {
+        Log.i(TAG, "Creating tf-idf vectors");
+        Set<String> terms = corpus.getInvertedIndex().keySet();
+
+        for (Document document : corpus.getDocuments()) {
+            HashMap<String, Double> weights = new HashMap<String, Double>();
+
+            for (String term : terms) {
+                double tf = document.getTermFrequency(term);
+                double idf = corpus.getInverseDocumentFrequency(term);
+                double weight = tf * idf;
+                weights.put(term, weight);
+            }
+            tfIdfWeights.put(document, weights);
+        }
+    }
+
+    /**
+     * This method returns the magnitude of a vector.
+     */
+    private double getMagnitude(Document document) {
+        double magnitude = 0;
+        HashMap<String, Double> weights = tfIdfWeights.get(document);
+
+        for (double weight : weights.values()) {
+            magnitude += weight * weight;
+        }
+
+        return Math.sqrt(magnitude);
+    }
+
+    /**
+     * This function takes two documents and return the dot product.
+     */
+    private double getDotProduct(Document d1, Document d2) {
+        double product = 0;
+        HashMap<String, Double> weights1 = tfIdfWeights.get(d1);
+        HashMap<String, Double> weights2 = tfIdfWeights.get(d2);
+
+        for (String term : weights1.keySet()) {
+            product += weights1.get(term) * weights2.get(term);
+        }
+
+        return product;
+    }
+
+    /**
+     * Returns the cosine similarity of two documents.
+     * Ranges from 0 (not similar) to 1 (very similar).
+     */
+    public double cosineSimilarity(Document d1, Document d2) {
+//		Log.i(TAG, "dot product: " + getDotProduct(d1, d2) +
+//				"magnitude of d1:" + getMagnitude(d1) + " mag of d2: " + getMagnitude(d2));
+        return getDotProduct(d1, d2) / (getMagnitude(d1) * getMagnitude(d2));
+    }
+}
